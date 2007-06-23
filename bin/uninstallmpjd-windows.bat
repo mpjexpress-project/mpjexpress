5d3a441e33d58a3dd5fdf3767d54ea3cacc743cf
shafia <aamir.shafi@gmail.com>
2007-06-23 11:51:40 +0000

    commiting the new windows scripts - the naming scheme remains the same ...

--- bin/uninstallmpjd-windows.bat
+++ bin/uninstallmpjd-windows.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 
+rem Copyright (c) 1999, 2006 Tanuki Software Inc.
 rem
 rem Java Service Wrapper general NT service uninstall script
 rem
@@ -15,7 +16,21 @@ rem Find the application home.
 rem
 rem %~dp0 is location of current script under NT
 set _REALPATH=%~dp0
-set _WRAPPER_EXE=%_REALPATH%Wrapper.exe
+
+rem Decide on the wrapper binary.
+set _WRAPPER_BASE=wrapper
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
+if exist "%_WRAPPER_EXE%" goto conf
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
+if exist "%_WRAPPER_EXE%" goto conf
+set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
+if exist "%_WRAPPER_EXE%" goto conf
+echo Unable to locate a Wrapper executable using any of the following names:
+echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
+echo %_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
+echo %_REALPATH%%_WRAPPER_BASE%.exe
+pause
+goto :eof
 
 rem
 rem Find the wrapper.conf
