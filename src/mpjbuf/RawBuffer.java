fe71e94692a0aa693ce574714f3e317f29fa7fcb
shafia <aamir.shafi@gmail.com>
2008-04-24 07:08:45 +0000

    adding 2008 in the copyright note of the source code

--- src/mpjbuf/RawBuffer.java
+++ src/mpjbuf/RawBuffer.java
@@ -3,11 +3,11 @@
 /*
 The MIT License
 
- Copyright (c) 2005 - 2007
+ Copyright (c) 2005 - 2008
    1. Distributed Systems Group, University of Portsmouth (2005)
-   2. Aamir Shafi (2005 - 2007)
-   3. Bryan Carpenter (2005 - 2007)
-   4. Mark Baker (2005 - 2007)
+   2. Aamir Shafi (2005 - 2008)
+   3. Bryan Carpenter (2005 - 2008)
+   4. Mark Baker (2005 - 2008)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
