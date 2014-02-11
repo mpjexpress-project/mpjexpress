858e54cb648d3bd912529706ac9dec77fbf13141
aleemakhtar <aleem.akhtar@seecs.edu.pk>
2014-02-11 09:35:40 +0000

    Updated with adjusted Formatting

--- src/runtime/daemon/Wrapper.java
+++ src/runtime/daemon/Wrapper.java
@@ -40,34 +40,33 @@ package runtime.daemon;
 import java.util.*;
 import java.net.*;
 import java.io.*;
-import java.lang.reflect.* ;
+import java.lang.reflect.*;
 
-public class Wrapper  extends Thread{
+public class Wrapper extends Thread {
 
   String configFileName = null;
   int processes = 0;
-  String className = null ;
+  String className = null;
   Class c = null;
   String deviceName = null;
-  String rank = null ; 
-  String [] nargs = null ; 
-  String hostName = null ;
+  String rank = null;
+  String[] nargs = null;
+  String hostName = null;
   String args[] = null;
-  public Wrapper(ThreadGroup group,String name) {
-  super(group,name);
+
+  public Wrapper(ThreadGroup group, String name) {
+    super(group, name);
   }
 
   /**
-   * Executes MPJ program in a new JVM. This method is invoked in main 
-   * method of this class, which is started by MPJDaemon. This method 
-   * can start multiple threads in a JVM. This will also parse
-   * configuration file.
-   * @param args Arguments to this method. 
-   *           args[0] is configFileName 'String', 
-   *           args[1] is number of processes, 
-   *           args[2] is deviceName, 
-   *           args[3] is rank, 
-   *           args[4] is className 
+   * Executes MPJ program in a new JVM. This method is invoked in main method of
+   * this class, which is started by MPJDaemon. This method can start multiple
+   * threads in a JVM. This will also parse configuration file.
+   * 
+   * @param args
+   *          Arguments to this method. args[0] is configFileName 'String',
+   *          args[1] is number of processes, args[2] is deviceName, args[3] is
+   *          rank, args[4] is className
    */
   public void execute(String args[]) throws Exception {
 
@@ -77,60 +76,65 @@ public class Wrapper  extends Thread{
     configFileName = args[0];
     processes = (new Integer(args[1])).intValue();
     deviceName = args[2];
-    rank = args[3] ; 
+    rank = args[3];
     className = args[4];
 
-    nargs = new String[ (args.length-5) ] ;
-    System.arraycopy(args, 5, nargs, 0, nargs.length) ;
+    nargs = new String[(args.length - 5)];
+    System.arraycopy(args, 5, nargs, 0, nargs.length);
 
-    c = Class.forName(className) ; 
+    c = Class.forName(className);
 
     try {
-      System.out.println("Starting process <"+rank+"> on <"+hostName+">");
+      System.out.println("Starting process <" + rank + "> on <" + hostName
+          + ">");
 
-      String arvs[] = new String[nargs.length+3]; 
+      String arvs[] = new String[nargs.length + 3];
 
       arvs[0] = rank;
       arvs[1] = configFileName;
       arvs[2] = deviceName;
-            
-      for(int i=0 ; i<nargs.length ; i++) {
-        arvs[i+3] = nargs[i];
+
+      for (int i = 0; i < nargs.length; i++) {
+        arvs[i + 3] = nargs[i];
       }
 
-      Method m = c.getMethod("main", new Class[] {arvs.getClass()});
+      Method m = c.getMethod("main", new Class[] { arvs.getClass() });
       m.setAccessible(true);
       int mods = m.getModifiers();
-      if (m.getReturnType() != void.class || !Modifier.isStatic(mods) ||
-                                             !Modifier.isPublic(mods)) {
-		  throw new NoSuchMethodException("main");
+      if (m.getReturnType() != void.class || !Modifier.isStatic(mods)
+          || !Modifier.isPublic(mods)) {
+        throw new NoSuchMethodException("main");
       }
 
-      m.invoke(null, new Object[] {arvs});
+      m.invoke(null, new Object[] { arvs });
 
-      System.out.println("Stopping process <"+rank+"> on <"+hostName+">");
-    }
-    catch (Exception ioe) {
-        System.out.println("multi-threaded starter: exception"
-                           + ioe.getMessage());
-        ioe.printStackTrace();
+      System.out.println("Stopping process <" + rank + "> on <" + hostName
+          + ">");
+    } catch (Exception ioe) {
+      System.out
+          .println("multi-threaded starter: exception" + ioe.getMessage());
+      ioe.printStackTrace();
     }
 
-  } 
-  public void run(){
-		try{
-	    execute(args);
-		}catch(Exception ex){ex.printStackTrace();}
   }
-  public static void main(String args[]) throws Exception {
-   // Wrapper wrap = new Wrapper();
-   // wrap.execute(args);
 
-	ThreadGroup group = new ThreadGroup("MPI"+args[3]);
-	Wrapper wrap = new Wrapper(group,args[3]);
-	wrap.args = args;
-	//Thread runner = new Thread("Main",wrap,group);
-	wrap.start();
-	wrap.join();
-  } 
+  public void run() {
+    try {
+      execute(args);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static void main(String args[]) throws Exception {
+    // Wrapper wrap = new Wrapper();
+    // wrap.execute(args);
+
+    ThreadGroup group = new ThreadGroup("MPI" + args[3]);
+    Wrapper wrap = new Wrapper(group, args[3]);
+    wrap.args = args;
+    // Thread runner = new Thread("Main",wrap,group);
+    wrap.start();
+    wrap.join();
+  }
 }
