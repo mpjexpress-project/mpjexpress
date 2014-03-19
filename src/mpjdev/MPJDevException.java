e5d75b590d7b204c149c8fea8b55f3819d03a3cb
bibrak <bibrakc@gmail.com>
2014-03-19 12:28:28 +0000

    provided a comment section to tell that that XDevException is not currently supported by natmpjdev. Have to think about a way ...

--- src/mpjdev/MPJDevException.java
+++ src/mpjdev/MPJDevException.java
@@ -7,47 +7,36 @@
    3. Bryan Carpenter (2005 - 2008)
    4. Mark Baker (2005 - 2008)
 
- Permission is hereby granted, free of charge, to any person obtaining
- a copy of this software and associated documentation files (the
- "Software"), to deal in the Software without restriction, including
- without limitation the rights to use, copy, modify, merge, publish,
- distribute, sublicense, and/or sell copies of the Software, and to
- permit persons to whom the Software is furnished to do so, subject to
- the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
- The above copyright notice and this permission notice shall be included
- in all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
- NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
- THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-/*
- * File         : MPJDevException.java
- * Author       : Aamir Shafi, Bryan Carpenter
- * Created      : Sat Oct 9 12:22:15 BST 2004
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ *
+ * File         : MPJException.java
+ * Author       : Bryan Carpenter
+ * Created      : Fri Nov 15 23:28:05 EST 2002
  * Revision     : $Revision: 1.3 $
- * Updated      : $Date: 2005/07/29 14:03:10 $
- *    
+ * Updated      : $Date: 2005/06/14 09:23:47 $
  */
+
+
 
 package mpjdev;
 
 import xdev.XDevException;
 
-public class MPJDevException 
-	       extends XDevException {
+public class MPJDevException extends XDevException {
 
+  //XDevException not to be thrown by natmpjdev
+  // at the moment we leave this as is
   public MPJDevException() {
-   super();
+    super();
   }
-  
+
   public MPJDevException(Throwable cause) {
-   super(cause);
+    super(cause);
   }
 
   public MPJDevException(String s) {
