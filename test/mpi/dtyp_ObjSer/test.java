a93397cd59a4e042c7569b390dc3d76ecc7e9312
bibrak <bibrakc@gmail.com>
2014-03-19 20:23:50 +0000

    provided main() method to be used for natmpjdev runtest.sh

--- test/mpi/dtyp_ObjSer/test.java
+++ test/mpi/dtyp_ObjSer/test.java
@@ -1,10 +1,10 @@
-package mpi.dtyp_ObjSer; 
+package mpi.dtyp_ObjSer;
 
 import java.io.*;
-public class test implements Serializable{
+
+public class test implements Serializable {
 
   public int a = 112;
   public String b = "test1";
   public int c = 999;
 }
-
