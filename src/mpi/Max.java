becfdd44d4eca0923a6dbf472f4476d4a44c49ce
bibrak <bibrakc@gmail.com>
2014-03-20 06:24:42 +0000

    initializing this.opCode = mpjdev.Constants.MAX_CODE

--- src/mpi/Max.java
+++ src/mpi/Max.java
@@ -32,20 +32,18 @@
  * Author       : Aamir Shafi, Bryan Carpenter
  * Created      : Fri Sep 10 12:22:15 BST 2004
  * Revision     : $Revision: 1.6 $
- * Updated      : $Date: 2005/07/29 14:03:09 $
+ * Updated      : $Date: 2014/03/11 13:26:15 $
  */
-
 
 package mpi;
 
 import mpjbuf.*;
 
-public class Max
-    extends Op {
+public class Max extends Op {
 
   Max() {
     worker = new MaxWorker();
+    this.opCode = mpjdev.Constants.MAX_CODE;
   }
 
 }
-
