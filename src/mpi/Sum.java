508e1832ffc21259ed9a82499a167cc3fb79c0c8
bibrak <bibrakc@gmail.com>
2014-03-20 06:18:41 +0000

    initializing this.opCode = mpjdev.Constants.SUM_CODE

--- src/mpi/Sum.java
+++ src/mpi/Sum.java
@@ -32,17 +32,17 @@
  * Author       : Aamir Shafi, Bryan Carpenter
  * Created      : Fri Sep 10 12:22:15 BST 2004
  * Revision     : $Revision: 1.6 $
- * Updated      : $Date: 2005/07/29 14:03:10 $
+ * Updated      : $Date: 2014/03/11 13:26:15 PKT $
  */
 
 package mpi;
 
 import mpjbuf.*;
 
-public class Sum
-    extends Op {
+public class Sum extends Op {
 
   Sum() {
     worker = new SumWorker();
+    this.opCode = mpjdev.Constants.SUM_CODE;
   }
 }
