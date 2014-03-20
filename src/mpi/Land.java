21f4dd5f67d55d89a525ae8a71687abda346213d
bibrak <bibrakc@gmail.com>
2014-03-20 06:25:46 +0000

    initializing this.opCode = mpjdev.Constants.LAND_CODE

--- src/mpi/Land.java
+++ src/mpi/Land.java
@@ -32,19 +32,18 @@
  * Author       : Aamir Shafi, Bryan Carpenter
  * Created      : Fri Sep 10 12:22:15 BST 2004
  * Revision     : $Revision: 1.6 $
- * Updated      : $Date: 2005/07/29 14:03:09 $
+ * Updated      : $Date: 2014/03/11 13:26:15 PKT $
  */
 
 package mpi;
 
 import mpjbuf.*;
 
-public class Land
-    extends Op {
+public class Land extends Op {
 
   Land() {
     worker = new LandWorker();
+    this.opCode = mpjdev.Constants.LAND_CODE;
   }
 
 }
-
