bb27be65515f5dfef4a84deaf6ce07316407310f
bibrak <bibrakc@gmail.com>
2014-04-29 11:57:52 +0000

    Merging the new rutime bin directory

--- bin/mpjdaemon.bat
+++ bin/mpjdaemon.bat
@@ -1,40 +1,26 @@
 @echo off
-setlocal
 
-rem
-rem Java Service Wrapper general startup script
-rem
+set count=0
+for %%i in (%*) do set /A count+=1
 
-rem
-rem Resolve the real path of the Wrapper.exe
-rem  For non NT systems, the _REALPATH and _WRAPPER_CONF values
-rem  can be hard-coded below and the following test removed.
-rem
-if "%OS%"=="Windows_NT" goto nt
-echo This script only works with NT-based versions of Windows.
+if NOT %count% == 1 goto :notValid
+
+
+if %1 == -boot (
+java -jar %MPJ_HOME%/lib/daemonmanager.jar -winboot
 goto :eof
+)
 
-:nt
-rem
-rem Find the application home.
-rem
-rem %~dp0 is location of current script under NT
-set _REALPATH=%~dp0
-set _WRAPPER_EXE=%_REALPATH%Wrapper.exe
+if %1 == -halt (
+java -jar %MPJ_HOME%/lib/daemonmanager.jar -winhalt 
+goto :eof
+)
 
-rem
-rem Find the wrapper.conf
-rem
-:conf
-set _WRAPPER_CONF="%~f1"
-if not %_WRAPPER_CONF%=="" goto startup
-set _WRAPPER_CONF="%_REALPATH%..\conf\wrapper.conf"
+if %1 == -status (
+java -jar %MPJ_HOME%/lib/daemonmanager.jar -status -hosts localhost
+goto :eof
+)
 
-rem
-rem Start the Wrapper
-rem
-:startup
-"%_WRAPPER_EXE%" -c %_WRAPPER_CONF%
-if not errorlevel 1 goto :eof
-pause
-
+:notValid
+echo "Usage: mpjdaemon.bat { -boot | -halt | -status }"
+goto :eof
